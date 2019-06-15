d65238640ecd69cbf012842355ba37e6f64e8d6b
showtara <shoara0509@gmail.com>
2019-06-16 01:25:27 +0900

    update sample

--- sample/src/main/java/io/github/aosa4054/pictureinpicturelifecycleobserver/sample/PIPActivity.kt
+++ sample/src/main/java/io/github/aosa4054/pictureinpicturelifecycleobserver/sample/PIPActivity.kt
@@ -1,10 +1,19 @@
 package io.github.aosa4054.pictureinpicturelifecycleobserver.sample
 
+import android.app.PendingIntent
 import android.app.PictureInPictureParams
+import android.app.RemoteAction
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.res.Configuration
+import android.graphics.Rect
+import android.graphics.drawable.Icon
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Rational
 import android.view.View
 import android.widget.Toast
 import io.github.aosa4054.pictureinpicturelifecycleobserver.PictureInPictureLifecycleObserver
@@ -12,7 +21,12 @@ import kotlinx.android.synthetic.main.activity_pip.*
 
 class PIPActivity : AppCompatActivity() {
 
-    lateinit var pipLifecycleObserver: PictureInPictureLifecycleObserver
+    companion object {
+        const val PIP_ACTION = "pip action"
+    }
+
+    private lateinit var pipLifecycleObserver: PictureInPictureLifecycleObserver
+    private var pipReceiver : BroadcastReceiver? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +34,29 @@ class PIPActivity : AppCompatActivity() {
 
         fab.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                enterPictureInPictureMode(PictureInPictureParams.Builder().build())
+
+                val params = PictureInPictureParams.Builder()
+                    .setAspectRatio(Rational(2, 1))
+                    .setSourceRectHint(Rect(1, 1, 1, 1))
+                    .setActions(
+                        listOf(
+                            RemoteAction(
+                                Icon.createWithResource(this, android.R.drawable.ic_dialog_dialer),
+                                "title",
+                                "description",
+                                PendingIntent.getBroadcast(this, 0, Intent(PIP_ACTION), 0)
+                            )
+                        )
+                    )
+                    .build()
+                enterPictureInPictureMode(params)
             } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 enterPictureInPictureMode()
             }
         }
 
         pipLifecycleObserver = object : PictureInPictureLifecycleObserver(this) {
-            override val finishDuplicatedTask = true
+            override val removeTaskAfterDismiss = true
 
             override fun onReturnFromPictureInPicture() {
                 Toast.makeText(this@PIPActivity, "Returned", Toast.LENGTH_SHORT).show()
@@ -41,6 +70,22 @@ class PIPActivity : AppCompatActivity() {
 
     override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean, newConfig: Configuration?) {
         super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
-        fab.visibility = if (isInPictureInPictureMode) View.GONE else View.VISIBLE
+        if (isInPictureInPictureMode) {
+            supportActionBar!!.hide()
+            fab.visibility = View.GONE
+
+            pipReceiver = object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    if (intent == null || intent.action != PIP_ACTION) return
+                    Toast.makeText(this@PIPActivity, "RemoteAction", Toast.LENGTH_SHORT).show()
+                }
+            }
+            registerReceiver(pipReceiver, IntentFilter(PIP_ACTION))
+        } else {
+            fab.visibility = View.VISIBLE
+
+            unregisterReceiver(pipReceiver)
+            pipReceiver = null
+        }
     }
 }
